87300bed22e5c47ec1c09031bd0cefb9f460d601
Shriharsh <shriharsh.dev@gmail.com>
2024-01-22 11:57:14 +0530

    Modifed to add delete and update routes

--- backend/routes/vendorRoutes.js
+++ backend/routes/vendorRoutes.js
@@ -3,15 +3,19 @@ const router = express.Router()
 const Vendor = require('../models/vendors')
 const vendorController = require('../controllers/vendorController')
 
-
+//GET all Vendors
 router.get('/vendors',vendorController.getVendors)
+
+//GET a single Vendor
 router.get('/:id',vendorController.getVendor)
+
+//POST a vendor
 router.post('/add', vendorController.createVendor)
 
-router.delete('/:id',(req,res) => {
-    res.send("Delete a vendor...")
-})
-router.patch('/:id',(req,res) => {
-    res.send("Update a vendor...")
-})
+//DELELTE a vendor
+router.delete('/:id',vendorController.deleteVendor)
+
+//UPDATE a vendor
+router.patch('/:id',vendorController.updateVendor)
+
 module.exports = router
